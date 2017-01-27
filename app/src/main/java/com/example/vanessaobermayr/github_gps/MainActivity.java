2c8906bb87f4572e345aa162b59b23b3750e4fef
vanessaobermayr <vanessa.obermayr@gmail.com>
2017-01-27 13:17:18 +0100

    neuFuerSimon

--- app/src/main/java/com/example/vanessaobermayr/github_gps/MainActivity.java
+++ app/src/main/java/com/example/vanessaobermayr/github_gps/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.vanessaobermayr.github_gps;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static LocationManager locman = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locman =(LocationManager) getSystemService(LOCATION_SERVICE);
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        locman.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,this);
+
+
+
+
+
     }
 }
